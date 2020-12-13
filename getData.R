692aa51078a4645034812efa0ef95200fca529cb
Ege Or <35030660+egeor90@users.noreply.github.com>
2020-12-13 20:27:37 +0300

    Update getData.R

--- getData.R
+++ getData.R
@@ -1,14 +1,17 @@
 start_ <- Sys.time()
-source("/Users/Ege/Google Drive/ABROAD/University of Warsaw/Free Works/maxfm/functions.R")
 
+setwd("/path/to/maxfm") # path must be defined
+source(paste0(getwd(),"/functions.R"))
+
+# Run the commented out lines once. Then comment them out
 # a <- getData("http://www.maxfm.com.tr/")
 # write.table(file="plist.csv", a, sep = ",")
 
 # Repeat every 5 mins
-b <- rbind(read.csv("/Users/Ege/Google Drive/ABROAD/University of Warsaw/Free Works/maxfm/plist.csv"), getData("http://www.maxfm.com.tr/"))
+b <- rbind(read.csv(paste0(getwd(),"/plist.csv")), getData("http://www.maxfm.com.tr/"))
 b <- b[!duplicated(b),]
 
-write.table(file="/Users/Ege/Google Drive/ABROAD/University of Warsaw/Free Works/maxfm/plist.csv", b, sep = ",")
+write.table(file=paste0(getwd(),"/plist.csv"), b, sep = ",")
 time_ <- Sys.time() - start_
 
 paste0(Sys.time(), ": process successful and it took ", time_, " seconds.")
