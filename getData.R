52ccc708fdbb2831040e104680e5b5630e80b323
Ege Or <35030660+egeor90@users.noreply.github.com>
2020-12-13 20:44:11 +0300

    Update getData.R

--- getData.R
+++ getData.R
@@ -1,17 +1,19 @@
 start_ <- Sys.time()
 
-setwd("/path/to/maxfm") # path must be defined
+setwd("/path/to/maxfm/") # path must be defined
 source(paste0(getwd(),"/functions.R"))
 
-# Run the commented out lines once. Then comment them out
-# a <- getData("http://www.maxfm.com.tr/")
-# write.table(file="plist.csv", a, sep = ",")
-
-# Repeat every 5 mins
-b <- rbind(read.csv(paste0(getwd(),"/plist.csv")), getData("http://www.maxfm.com.tr/"))
-b <- b[!duplicated(b),]
+if(!file.exists(paste0(getwd(), "/plist.csv"))){
+  b <- getData("http://www.maxfm.com.tr/")
+  write.table(file="plist.csv", b, sep = ",")
+}else if(file.exists(paste0(getwd(), "/plist.csv"))){
+  b <- rbind(read.csv(paste0(getwd(),"/plist.csv")), getData("http://www.maxfm.com.tr/"))
+  b <- b[!duplicated(b),]
+}else{
+  print("Error!")
+}
 
 write.table(file=paste0(getwd(),"/plist.csv"), b, sep = ",")
-time_ <- Sys.time() - start_
 
+time_ <- Sys.time() - start_
 paste0(Sys.time(), ": process successful and it took ", time_, " seconds.")
